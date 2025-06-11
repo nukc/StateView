fcca934447bd00e6d93af3d20c9f3e9a3e23bd90
C <bocause@qq.com>
2025-06-11 15:24:35 +0800

    fix(library): attr

--- library/src/main/java/com/github/nukc/stateview/StateView.java
+++ library/src/main/java/com/github/nukc/stateview/StateView.java
@@ -524,7 +524,7 @@ public class StateView extends View {
     public int getActionBarHeight() {
         int height = 0;
         TypedValue tv = new TypedValue();
-        if (getContext().getTheme().resolveAttribute(R.attr.actionBarSize, tv, true)) {
+        if (getContext().getTheme().resolveAttribute(android.R.attr.actionBarSize, tv, true)) {
             height = TypedValue.complexToDimensionPixelSize(tv.data, getResources().getDisplayMetrics());
         }
         return height;
